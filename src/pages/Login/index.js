2a30b2e2577c708d3c828bfc3f6c63c8c922b4da
Thiago Yudi Fukunaga <thiagoyudifukunaga@gmail.com>
2019-06-12 16:40:16 -0400

    Feature autentication

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,61 @@
 import React, { Component } from 'react';
+import api from '~/services/api';
 
-import { View } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as LoginActions from '~/store/actions/login';
 
-export default class Login extends Component {
+import {
+  Container, Input, Button, ButtonText, Error,
+} from './styles';
+
+class Login extends Component {
+  state = {
+    username: '',
+  };
+
+  handleSumit = async () => {
+    const { username } = this.state;
+    const { loginSuccess, loginFailure, navigation } = this.props;
+
+    try {
+      await api.get(`/users/${username}`);
+      loginSuccess(username);
+      navigation.navigate('Repositories');
+    } catch (err) {
+      loginFailure();
+    }
+  };
+
   render() {
-    return <View />;
+    const { username } = this.state;
+    const { error } = this.props;
+
+    return (
+      <Container>
+        {error && <Error>Usuário inexistente</Error>}
+        <Input
+          value={username}
+          onChangeText={text => this.setState({ username: text })}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Digite seu usuário"
+        />
+        <Button onPress={this.handleSumit}>
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+      </Container>
+    );
   }
 }
+
+const mapStateToProps = state => ({
+  error: state.login.error,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(LoginActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Login);
