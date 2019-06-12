790698838ce727c5a04700c573b00b5bacb821ae
Thiago Yudi Fukunaga <thiagoyudifukunaga@gmail.com>
2019-06-12 13:55:06 -0400

    Learning dispatch from Redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,19 @@
 import { createStore } from 'redux';
 
 // Reducer
-function reducer() {
-  return ['Fazer café', 'Estudar React Native'];
+const INITIAL_STATE = ['Fazer café', 'Estudar React Native'];
+
+// Actions
+
+// Adicionar um todo
+// Marcar todo como completo
+
+function reducer(state = INITIAL_STATE, action) {
+  if (action.type === 'ADD_TODO') {
+    return [...state, action.text];
+  }
+
+  return state;
 }
 
 const store = createStore(reducer);
